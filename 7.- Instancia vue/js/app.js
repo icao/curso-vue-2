ec9717ba987140c6c5430138b72cf7859533980e
Cesar Aparicio <cesar.aparicio@amk-technologies.com>
2018-02-20 09:06:47 -0600

    Finalizando capitulo de instancia Vue

--- 7.- Instancia vue/js/app.js
+++ 7.- Instancia vue/js/app.js
@@ -11,13 +11,20 @@ const vm1 = new Vue({
   data: {
     mensaje: 'Hola instancia 1'
   },
+  beforeUpdate() {
+    console.log('Before update: ', this.mensaje);
+  },
+  updated() {
+    console.log('Updated:', this.mensaje);    
+  },
+
   // Metodos que se ocuparan en la instancia(manipular o muestran el modelo)
   methods: {
     alReves() {
       this.mensaje = this.mensaje.split('').reverse().join('');
       
       // Accediendo a la instancia vm2 desde la vm1
-      vm2.mensaje = 'Hola desde instancia 1';
+      // vm2.mensaje = 'Hola desde instancia 1';
     }
   },
   // Metodos para modificar el modelo y representar en la vista de un modo diferente
@@ -37,3 +44,12 @@ const vm2 = new Vue({
   }
 });
 
+/**
+ * Dentro del ciclo de vida de la instancia de Vue(),
+ * tenemos varios métodos que se ejecutan,
+ * para ver mas acerca de estos métodos, visitar la documentación oficial
+ * de Vue.js
+ * 
+ * Para ejecutar estos métodos se ponen directamente en la raíz de la 
+ * instancia, en este caso añadiremos beforeUpdate() y updated()
+ */
