ceb52571dc518e61baa1368cde518818bc03d8bd
Cesar Aparicio <cesar.aparicio@amk-technologies.com>
2018-02-23 14:21:54 -0600

    Adición de capitulo de transiciones

---
+++ 9.- Transiciones/js/app.js
@@ -0,0 +1,15 @@
+
+'use strict';
+
+new Vue({
+  el: 'main',
+  data: {
+    mostrar: 'true',
+    mensajes: {
+      transicion: 'Transiciones de clases CSS',
+      animacion: 'Animaciones CSS',
+      nimacionCustom: 'Animaciones custom CSS',
+      entreElementos: 'Transiciones entre elementos Vue'
+    }
+  }
+});
