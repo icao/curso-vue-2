111e761ed17c6268d8c16dbf6dcf4dfd00f8253b
Cesar Aparicio <cesar.aparicio@amk-technologies.com>
2018-03-09 13:53:33 -0600

    Finalizando capitulo introduccion a componentes

---
+++ 12.- Introducción a componentes/js/app.js
@@ -0,0 +1,68 @@
+
+'use strict';
+
+Vue.component('mis-tareas', {
+  //No se declaran propiedades, porque busca la data dentro del modelo
+  template: `<ul><li v-for="tarea in tareas">{{tarea.title}}</li></ul>`,
+  mounted() { 
+    axios.get('https://jsonplaceholder.typicode.com/todos')
+      .then((response) => {
+        this.tareas = response.data;
+      });
+  },
+  // Funcion data()
+  data() {
+    // El componente busca la fuente de datos ya desde dentro
+    return {
+      tareas: []
+    }
+  }
+});
+
+
+new Vue({
+  el: 'main'
+});
+
+
+/**
+ * Cuandose usan componentes, la forma de declarar el modelo cambia,
+ * en ves de tener un objeto mas como propiedad dentro de la instancia Vue,
+ * se declara dentro del componente como una función.
+ * 
+ * Los componentes tienen las mismas propiedades que la instancia Vue,
+ * por lo que igual pueden ser declaradas dentro de un componente.
+ */
+
+
+// ---------------------------------------------------------------------------
+
+/**
+ * Un oomponente puede ser con entradas dinamicas usando la instancia de Vue,
+ * aunque no es del todo una buena forma de tener a nuestros componentes.
+ * Ejemplo:  
+ */
+
+// Vue.component('mis-tareas', {
+//   props: ['tareas'],
+//   template: `<ul><li v-for="tarea in tareas">{{tarea.title}}</li></ul>`
+// });
+// // Este compoenente es Agnostico, no importa cual sea el listado de entrada de tareas
+
+// new Vue({
+//   el: 'main',
+//   mounted() { 
+//     axios.get('https://jsonplaceholder.typicode.com/todos')
+//       .then((response) => {
+//         this.tareasAjax = response.data;
+//       });
+//   },
+//   data: {
+//     tareasAjax: [],
+//     tareasLocales: [
+//       {title:'Hacer la compra'},
+//       {title:'Ir al gimnasio'},
+//       {title:'Estudiar Vue haciendo Deep Work'}
+//     ]
+//   }
+// });
