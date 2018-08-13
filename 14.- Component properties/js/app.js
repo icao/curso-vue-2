e5d79ee3517bf9f5eefb9ba706c1be8d281a02e2
ACO-EC-009 <cesar.aparicio@amk-technologies.com>
2018-08-13 10:10:22 -0500

    Finalizando propiedades de las Prop's

--- 14.- Component properties/js/app.js
+++ 14.- Component properties/js/app.js
@@ -14,7 +14,7 @@ Vue.component('creador', {
             </section>`,
   methods: {
     cambiarProp() {
-      this.nombre = this.nombre.toUpperCase();
+      this.nombre = this.nombre.toUpperCase(); // Esto jamás tiene que hacerse.
     }
   }
 });
@@ -27,13 +27,39 @@ new Vue({
 });
 
 /**
+ * PROPIEDADES DE LAS PROPS
  * Un componente esta aislado del resto por lo que se necesita pasar información a él a travez de propiedades.
  * Las propiedades en este caso son pasadas desde el template.
  * Si estas son modificadas automaticamente los hijos se ven afectados.
- * Una restricción importante es que las propiedades jamás tienen que mutar "directamente dentro del componente", es decir, no tienen que modificarse.
- * 
+ * Una restricción importante es que las propiedades jamás tienen que mutar "directamente dentro del componente", 
+ * es decir, no tienen que modificarse.
  */
 
-// Si nosotros intentamos modificar una propiedad, como en este caso:   this.nombre = this.nombre.toUpperCase();
-// Vue nos notificara con un error de que la propiedad sera sobre escrita, no es buena idea porque
-// el data flow es de arriba a abajo, de padre a hijo.
+/**
+ * Si nosotros intentamos modificar una propiedad, como en este caso:
+ * this.nombre = this.nombre.toUpperCase();
+ * Vue nos notificara con un error de que la propiedad sera sobre escrita, 
+ * Esto no es una buena practica, ya que se contradice el data flow es de arriba a abajo, de padre a hijo.
+ * Culquier tipo de transformación directamente a las PROP's esta phohibido.
+ * 
+ * Hay usualmente dos casos en los que puede ser tentador modificar una propiedad:
+ * 1.- La propiedad es utilizada solo para dar un valor inicial, el componente hijo solo quiere utilizarla como una propiedad de datos local luego;
+ * 2.- La propiedad es pasada como un valor crudo que luego necesita ser transformado.
+ * 
+ * Las respuestas apropiadas a estos casos de uso son:
+ * 
+ * 1.- Define una propiedad de datos local que utilice el valor inicial de la propiedad como su valor inicial:
+ * props: ['initialCounter'],
+ * data: function () {
+ *  return { counter: this.initialCounter }  // Asociando el valor de la Prop a counter
+ * }
+ * 
+ * 2.- Define una propiedad computada que sea calculada en base al valor de la propiedad pasada:
+ * 
+ * props: ['size'],
+ * computed: {
+ *  normalizedSize: function () { // Propiedad computada para transformar el valor de la prop.
+ *    return this.size.trim().toLowerCase()
+ *  }
+ * }
+ */
