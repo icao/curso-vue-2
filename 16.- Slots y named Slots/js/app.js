7772e52e3303a1cb70601134f09f434e67b1ba51
César Aparicio <clark012389@hotmail.com>
2018-11-29 19:39:43 -0600

    Finalizando seccion de slots y named slots
    Finalizando seccion de uso se SLOTS

--- 16.- Slots y named Slots/js/app.js
+++ 16.- Slots y named Slots/js/app.js
@@ -1,12 +1,30 @@
 'use strict';
 
-
 Vue.component('alerta', {
-
-  template: '#alerta-template'
+  props: ['tipo', 'posicion'],
+  // template: '#alerta-template'
+  template: `
+          <section class="alerta" :class="[tipo, posicion]">
+            <header class="alerta__header">
+              <slot name="header">Soy un el HEADER</slot>
+            </header>
+            <div class="alerta__contenido">
+              <slot>Contenido por default :P</slot>
+            </div>
+            <footer class="alerta__footer">
+              <slot name="footer">Soy un FOOTER</slot>
+            </footer>
+          </section>`
 });
 
 new Vue({
-  el: 'main'
+  el: 'main',
+  //  NOTA 1: Si nosotros queremos pasar desde data evaluando los valores de
+  // las entradas de nuestras props, acudimos a asignarlas en un parametro dentro de nuestro
+  // modelo de datos:
+  // data: {
+  //   posicionData: 'alerta--abajo-derecha',
+  //   tipoData: 'alerta--exito'
+  // }
 });
 
