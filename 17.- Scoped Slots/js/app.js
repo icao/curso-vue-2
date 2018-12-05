9beac977009ec18bdb6bc1b57ab19a971c346927
César Aparicio <clark012389@hotmail.com>
2018-12-05 14:45:17 -0600

    Finalizando capitulo de Scoped Slots
    Concluimos el tema de scoped slots

--- 17.- Scoped Slots/js/app.js
+++ 17.- Scoped Slots/js/app.js
@@ -1,12 +1,19 @@
 'use strict';
 
 Vue.component('mis-tareas', {
-  template: `<h1>Hola Scoped Slots</h1>`
+  props: ['listado'],
+  template: '#template-listado',
 });
 
 new Vue({
-  el: 'main',
+  el: "main",
   data: {
-    mensaje: 'Hola mundo'
+    tareas: [
+      { actividad: "Bañarse", hora: "7am" },
+      { actividad: "Desayunar", hora: "8am" },
+      { actividad: "Hacer ejercicio", hora: "12pm" },
+      { actividad: "Prepararse la comida", hora: "3pm" },
+      { actividad: "Estudiar Vuejs", hora: "6pm" }
+    ]
   }
 });
