836a3e0890f609195dff6b1c41206029e4f01729
César Aparicio <clark012389@hotmail.com>
2018-12-15 15:52:49 -0600

    Finalizando segundo ejemplo de custom events
    Finalizando capitulo Custom Events

--- 19.- Custom Events/js/app.js
+++ 19.- Custom Events/js/app.js
@@ -1,6 +1,27 @@
+Vue.component('button-counter', {
+  template: '#template-button-counter',
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    incrementarContador() { 
+      this.counter += 1;
+      // Emite un evento al metodo incrementarTotal del padre
+      this.$emit('incrementar');
+    }
+  }
+});
+
 new Vue({
   el: 'main',
   data: {
-    mensaje: 'Ejemplo 2 Custom Event'
+    total: 0
+  },
+  methods: {
+    incrementarTotal() { 
+      this.total += 1
+    }
   }
 });
