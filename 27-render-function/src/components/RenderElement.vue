1e848bba4165b11397e964a292fab6c8210d8e12
César Aparicio <clark012389@hotmail.com>
2019-05-11 17:36:54 -0500

    Creando proyecto ccon componente base para sección de render funtion

---
+++ 27-render-function/src/components/RenderElement.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>😄</h1>
+    <section>
+      <h2>{{ msg }}</h2>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RenderElement",
+  props: {
+    msg: {
+      type: String
+    }
+  }
+};
+</script>
+
+<style>
+h1 {
+  font-size: 6em;
+}
+section {
+  border: 1px solid #3de475;
+}
+</style>
