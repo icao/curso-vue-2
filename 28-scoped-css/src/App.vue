c6f9786f744a9c80148b82ce202060a935ada886
César Aparicio <clark012389@hotmail.com>
2019-05-14 15:37:15 -0500

    Agregando e implementando archivos de custom css

---
+++ 28-scoped-css/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Tareas></Tareas>
+  </div>
+</template>
+
+<script>
+import Tareas from '@/components/Tareas'
+
+export default {
+  name: 'app',
+  components: {
+    Tareas
+  }
+}
+</script>
+
+<style scoped>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 28-scoped-css/src/components/Tareas.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <h2>Tareas Pendientes</h2>
+    <ul v-for="(tarea, index) in tareasPendientes" :key="tarea.actividad">
+      <li @click="actualizarTarea(tarea)">
+        {{ index + 1 }} {{ tarea.actividad }}
+      </li>
+    </ul>
+    <h2>Tareas Hechas</h2>
+    <ol v-for="(tarea, index) in tareasHechas" :key="tarea.actividad">
+      <li @click="actualizarTarea(tarea)">
+        {{ index + 1 }} {{ tarea.actividad }}
+      </li>
+    </ol>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Tareas',
+  data() {
+    return {
+      tareas: [
+        { actividad: 'Aprender Vuex', completado: true },
+        { actividad: 'Aprender Router', completado: false },
+        { actividad: 'Aprender Webpack + Vue', completado: false },
+        { actividad: 'Aprender Firebase', completado: true },
+        { actividad: 'Terminar este capitulo Vuex', completado: true },
+        { actividad: 'Hacer ejercicio de nuevo', completado: false },
+        { actividad: 'Ir al super a comprar', completado: false }
+      ]
+    }
+  },
+  computed: {
+    tareasHechas() {
+      return this.tareas.filter(tarea => tarea.completado === true)
+    },
+    tareasPendientes() {
+      return this.tareas.filter(tarea => tarea.completado === false)
+    }
+  },
+  methods: {
+    actualizarTarea(tarea) {
+      tarea.completado = !tarea.completado
+    }
+  }
+}
+</script>
+
+<style scoped>
+div {
+  background-color: palegreen;
+  border-radius: 5px;
+  padding: 2em;
+}
+ul li,
+ol li {
+  text-align: start;
+  cursor: pointer;
+  padding: 0.1em;
+  font-size: 1.05em;
+  list-style: none;
+}
+</style>
